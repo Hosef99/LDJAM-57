75fe88369db714acc31146daed665586e2984d8c
Tan Wai Hong <tan040724101445@gmail.com>
2025-04-05 23:00:19 +0800

    camera following update

--- Assets/Scripts/CameraFollowing.cs
+++ Assets/Scripts/CameraFollowing.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 public class CameraFollowing : MonoBehaviour
 {
     public Transform target;
-    private Transform previousTarget;
+    private Vector3 previousPosition;
     public float smoothSpeed = 0.125f;
-
+    public int camBuffer = 2;
     void Start ()
     {
         
         Camera.main.orthographicSize = 10f;
+        previousPosition = target.position;
     }
     void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + new Vector3(0f, -5.08f, -10f);
+        if (previousPosition.x < target.position.x - camBuffer)
+        {
+            previousPosition.x = target.position.x - camBuffer - 0.01f;
+        }else if (previousPosition.x > target.position.x + camBuffer)
+        {
+            previousPosition.x = target.position.x + camBuffer + - 0.01f;
+        }
+        Vector3 desiredPosition = previousPosition + new Vector3(0f, -5.08f, -10f);
+
         //Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = desiredPosition;
     }
